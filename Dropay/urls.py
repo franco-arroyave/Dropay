e4fe57ca6809773304f9360346486e2369fdb7d0
juancr15 <juan288cruz@gmail.com>
2022-04-24 21:20:31 -0500

    Log in added

--- Dropay/urls.py
+++ Dropay/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('login', views.login, name='login'),
+    path('', views.logIn, name='login'),
+    path('login', views.logIn, name='login'),
     path('home', views.index, name='index'),
     path('loans', views.loans, name='loans'),
     path('loans/newLoan', views.newLoan, name='newLoan'),
     path('loans/addPayment', views.loanPayment, name='loanPayment'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- Dropay/views.py
+++ Dropay/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "pages/index.html")
-def login(request):
-    return render(request, "pages/login.html")
 def loans(request):
     return render(request, 'loans/index.html')
 def newLoan(request):
     return render(request, 'loans/new.html')
 def loanPayment(request):
     return render(request, 'loans/payment.html')
+def logIn(request):
+    return render(request, 'pages/logIn.html')

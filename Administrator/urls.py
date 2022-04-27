8e9ccd45d0ea4d376cb3ae6a4084686e669dbd7a
Franco Arroyave <josefranco.ac@gmail.com>
2022-04-27 00:06:56 -0500

    edit URLs

---
+++ Administrator/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
+urlpatterns = [
+]
+
+urlpatterns += staticfiles_urlpatterns()

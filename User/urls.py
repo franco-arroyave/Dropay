07faf18d627b10d893669efc9e09ffbd33c1bb16
Franco Arroyave <josefranco.ac@gmail.com>
2022-04-27 00:05:09 -0500

    change URLs

---
+++ User/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
+urlpatterns = [
+
+]
+
+urlpatterns += staticfiles_urlpatterns()

7e97c477be46c7286a44765f9bad155b4090f046
Franco Arroyave <josefranco.ac@gmail.com>
2022-04-26 22:53:31 -0500

    Update apps and proyect

---
+++ Credit/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
+urlpatterns = [
+    path('', views.logIn, name='login'),
+    path('login', views.logIn, name='login'),
+    path('signup', views.signUp, name='signup'),
+    path('home', views.index, name='index'),
+    path('loans', views.loans, name='loans'),
+    path('loans/newLoan', views.newLoan, name='newLoan'),
+    path('loans/addPayment', views.loanPayment, name='loanPayment'),
+]
+
+urlpatterns += staticfiles_urlpatterns()

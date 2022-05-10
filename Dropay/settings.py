17056faec0cc75d88d1b34ea625ba391d95dd111
Franco Arroyave <josefranco.ac@gmail.com>
2022-05-09 22:36:08 -0500

    Update user model

--- Dropay/settings.py
+++ Dropay/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'User.xUser'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('index')
--- User/models.py
+++ User/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 class TypeID(models.Model):
     TypeIDID = models.AutoField(primary_key=True)
 
-class xUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class xUser(AbstractUser):
     IdentityDocument = models.IntegerField(null=True, verbose_name='Documento de Identidad')
     TypeID = models.ForeignKey('TypeID',on_delete=models.CASCADE)
     DateofBirth = models.DateTimeField(null=True, verbose_name='Cumpleaños')

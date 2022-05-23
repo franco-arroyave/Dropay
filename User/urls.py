122c9b1b9368036c7972dd2b65d47e4680b4d9a5
juancr15 <juan288cruz@gmail.com>
2022-05-22 22:57:44 -0500

    Loans index updated

--- User/urls.py
+++ User/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.views import LoginView, LogoutView, logout_then_login
 from . import views
+from Credit import views as viewsCredit
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='pages/login.html'), name='login'),
     path('accounts/login/', LoginView.as_view(template_name='pages/login.html')),
     path('logout', LogoutView.as_view(template_name='pages/login.html'), name='logout'),
     path('signup', views.signUp, name='signup'),
-    path('home', views.index, name='index'),
+    path('home', viewsCredit.loans, name='index'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
